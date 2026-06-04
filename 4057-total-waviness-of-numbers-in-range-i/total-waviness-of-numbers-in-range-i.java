152e0a5650e5d8d18cd93dea28501507c3ee390c
siddhant8840 <146637403+siddhant8840@users.noreply.github.com>
2026-06-04 07:11:47 +0530

    Time: 14 ms (90.71%) | Memory: 46.4 MB (67.29%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -3,17 +3,22 @@ class Solution {
         int ans = 0;
 
         for (int num = num1; num <= num2; num++) {
-            String s = String.valueOf(num);
+            int[] d = new int[10];
+            int len = 0, x = num;
 
-            for (int i = 1; i < s.length() - 1; i++) {
-                char left = s.charAt(i - 1);
-                char cur = s.charAt(i);
-                char right = s.charAt(i + 1);
+            while (x > 0) {
+                d[len++] = x % 10;
+                x /= 10;
+            }
+
+            for (int i = 1; i < len - 1; i++) {
+                int left = d[i + 1];
+                int cur = d[i];
+                int right = d[i - 1];
 
                 if ((cur > left && cur > right) ||
-                    (cur < left && cur < right)) {
+                    (cur < left && cur < right))
                     ans++;
-                }
             }
         }
 
